624a4b70bf6fe8ae21335e4bc363b70da30ca4d8
Tommi Santamaa <Tommi.Santamaa@gmail.com>
2019-02-27 13:05:50 +0200

    Replace icon, rename app title

--- src/index.js
+++ src/index.js
@@ -3,12 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 import 'semantic-ui-css/semantic.min.css'
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from './App'
+import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'))
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
